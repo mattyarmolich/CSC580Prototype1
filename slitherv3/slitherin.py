1d78518cacb0dbd4f179eea0dd17a1b92cfed8a8
Markus <markus_pielmeier@hotmail.de>
2019-05-02 16:05:29 -0700

    removed unneeded running options

--- slitherv3/slitherin.py
+++ slitherv3/slitherin.py
@@ -2,32 +2,18 @@ import argparse
 import random
 from game.helpers.constants import Constants
 from game.models.general_purpose.human_solver import HumanSolver
-from game.models.general_purpose.random_ai_solver import RandomSolver
-from game.models.general_purpose.monte_carlo_ai_solver import MonteCarloSolver
-from game.models.domain_specific.shortest_path_bfs_ai_solver import ShortestPathBFSSolver
-from game.models.domain_specific.shortest_path_dfs_ai_solver import ShortestPathDFSSolver
-from game.models.domain_specific.longest_path_ai_solver import LongestPathSolver
-from game.models.domain_specific.hamilton_ai_solver import HamiltonSolver
-from game.models.domain_specific.dnn_ai_solver import DNNSolver, DNNTrainer
-from game.models.domain_specific.dnn_monte_carlo_ai_solver import DNNMonteCarloSolver
+
 from game.models.domain_specific.dnn_genetic_evolution_ai_solver import DNNGeneticEvolutionSolver, DNNGeneticEvolutionTrainer
 from game.models.general_purpose.ddqn_ai_solver import DDQNSolver, DDQNTrainer
 
 
-solvers = [RandomSolver(),
+solvers = [
            HumanSolver(),
-           MonteCarloSolver(),
-           ShortestPathBFSSolver(),
-           ShortestPathDFSSolver(),
-           LongestPathSolver(),
-           HamiltonSolver(),
-           DNNSolver(),
-           DNNMonteCarloSolver(),
            DNNGeneticEvolutionSolver(),
            DDQNSolver()
            ]
 
-trainers = [DNNTrainer(),
+trainers = [
             DNNGeneticEvolutionTrainer(),
             DDQNTrainer()
             ]
